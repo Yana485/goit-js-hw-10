c8567004ad9424d47b380ec979cec3d255ffe8da
Yana485 <105270905+Yana485@users.noreply.github.com>
2026-05-03 21:50:00 +0100

    fixing libraries

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,12 +1,12 @@
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+// // Описаний в документації
+// import flatpickr from "flatpickr";
+// // Додатковий імпорт стилів
+// import "flatpickr/dist/flatpickr.min.css";
 
-// Описаний у документації
-import iziToast from "izitoast";
-// Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+// // Описаний у документації
+// import iziToast from "izitoast";
+// // Додатковий імпорт стилів
+// import "izitoast/dist/css/iziToast.min.css";
 
 let userSelectedDate;
 const startButton = document.querySelector("button[data-start]");
